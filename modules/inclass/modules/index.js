e1036e896fdecb9f7b2419d9da1270356198c813
BruceHill <brucehillwalley@gmail.com>
2024-02-26 20:41:21 +0300

    http (built in)

--- modules/inclass/modules/index.js
+++ modules/inclass/modules/index.js
@@ -3,3 +3,22 @@
 NODE MODULES
 */
 console.log("this line is in index.js");
+
+var fs = require('fs');
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+});
+
+console.log(fs.readFileSync('mynewfile1.txt', 'utf8'));
+
+fs.open('mynewfile2.txt', 'w', function (err, file) {
+    if (err) throw err;
+    console.log('Saved!');
+})
+
+fs.writeFile('mynewfile2.txt',"2.Hello content!", function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+})
