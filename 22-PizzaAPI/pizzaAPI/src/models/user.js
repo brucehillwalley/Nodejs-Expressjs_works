0817d8763e15bacba76faa03e2f9c3361608c901
BruceHill <brucehillwalley@gmail.com>
2024-03-25 22:34:50 +0300

    user model controller route added

--- 22-PizzaAPI/pizzaAPI/src/models/user.js
+++ 22-PizzaAPI/pizzaAPI/src/models/user.js
@@ -1,6 +1,63 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      //   set: passwordEncrypt,
+      //   validate: [
+      //     (password) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password),
+      //     'Password type is not correct.'
+      // ],
+      set: (password) => {
+        if (
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)
+        ) {
+          return passwordEncrypt(password);
+        } else {
+          throw new Error("Password type is not correct.");
+        }
+      },
+      validate: (password) => password != "wrong",
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Email type is incorrect",
+      ],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "users",
+  }
+);
+
+// Model:
+module.exports = mongoose.model("User", userSchema);
