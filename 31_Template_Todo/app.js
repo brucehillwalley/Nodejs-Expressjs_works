f32b26257c9b6f6d792f50631138151f01fc8075
BruceHill <brucehillwalley@gmail.com>
2024-04-24 21:23:50 +0300

    template lesson

---
+++ 31_Template_Todo/app.js
@@ -0,0 +1,47 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+// Accept json data and convert object:
+app.use(express.json())
+
+// Catch async-errors:
+require('express-async-errors')
+/* ------------------------------------------------------- */
+//* TEMPLATE-EJS:
+// $ npm i ejs
+// https://ejs.co/
+// https://www.npmjs.com/package/ejs
+// https://github.com/mde/ejs/wiki/Using-EJS-with-Express
+
+// console.log(app);
+//?view engine i tanıttık
+app.set("view engine", "ejs");
+
+app.all('/',(req,res)=>{
+    //API:
+    // res.send({message:'hello'})
+    // View Template:
+    // res.render('index.ejs')
+    res.render('index')
+})
+
+
+/* ------------------------------------------------------- */
+// Routes:
+
+app.use(require('./app/routes/todo.router'))
+
+/* ------------------------------------------------------- */
+// ErrorHandler:
+app.use(require('./app/errorHandler'))
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
