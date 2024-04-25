87c038814542d9f6ba73658fdead31e338ea0af1
BruceHill <brucehillwalley@gmail.com>
2024-04-26 01:29:38 +0300

    error view

--- 31_Template_Todo/app/errorHandler.js
+++ 31_Template_Todo/app/errorHandler.js
@@ -5,24 +5,23 @@
 
 module.exports = (err, req, res, next) => {
     const errorStatusCode = res.errorStatusCode ?? 500
+
     console.log('errorHandler worked.')
 
-    // console.log(req.originalUrl);
+    // console.log(req.originalUrl)
 
     const data = {
         error: true, // special data
         message: err.message, // error string message
         cause: err.cause, // error option cause
         // stack: err.stack, // error details
-    
-    
     }
-        
-    
 
-    if(req.originalUrl.startsWith('/api')) {
-    res.status(errorStatusCode).send(data)
-} else {
-    res.render('errors', data)
-}
+    if (req.originalUrl.startsWith('/api')) {
+    // API -> JSON:
+        res.status(errorStatusCode).send(data)
+    } else {
+    // VIEW -> HTML
+        res.render('errors', { data })
+    }
 }
